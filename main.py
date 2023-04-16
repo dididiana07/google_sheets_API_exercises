5356222a55b8bd087a5fba5209a35610ea1b68a1
Diana <102248347+dididiana07@users.noreply.github.com>
2023-04-16 16:32:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import datetime
+import os
+import requests
+
+NUTRITIONIX_ID = os.environ["NUTRITIONIX_ID"]
+NUTRITIONIX_API_KEY = os.environ["NUTRITIONIX_API_KEY"]
+sheety_authorization = os.environ["SHEETY_AUTH"]
+
+headers = {
+    "x-app-id": f"{NUTRITIONIX_ID}",
+    "x-app-key": f"{NUTRITIONIX_API_KEY}",
+}
+
+sheety_headers = {
+    "Authorization": f"{sheety_authorization}"
+}
+parameters = {
+    "age": "23",
+    "gender": "female",
+    "weight_kg": f"{45}",
+    "query": f"{input('Tell me what exercises you did: ')}"
+}
+
+
+def nutritionix_exercises(request_headers, request_body):
+    """Provide json values that correspond to the header and the request_body"""
+    nutritionix_response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise",
+                                         headers=request_headers,
+                                         json=request_body)
+    data = nutritionix_response.json()["exercises"]
+    return data
+
+
+def current_date_time():
+    """Returns a tuple of current day and time converted to strings."""
+    date = datetime.datetime.today().date().strftime("%d/%m/%Y")
+    time = datetime.datetime.today().time().strftime("%H:%M:%S")
+    return date, time
+
+
+current_date, current_time = current_date_time()
+exercises = nutritionix_exercises(request_headers=headers, request_body=parameters)
+
+for exercise in exercises:
+    exercise_name = exercise["name"]
+    duration = exercise["duration_min"]
+    calories = exercise["nf_calories"]
+    sheety_parameters = {
+        "workout":
+            {
+                "date": current_date,
+                "time": current_time,
+                "exercise": exercise_name,
+                "duration": duration,
+                "calories": calories
+            }
+    }
+    sheety_response = requests.post(url="https://api.sheety.co/1022e180f0ac414bc9f54f2e2113f351/myWorkouts/workouts",
+                                    headers=sheety_headers, json=sheety_parameters)
